210ca8e91bfd60aea8058e69868e75becdba4f97
Titov <dtitov1987@gmail.com>
2018-12-09 10:38:54 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let num = 123;
+console.log(num);
+
+let arr = ['one','two','three'];
+
+arr.forEach(element => {
+   console.log(element); 
+});
+
+let answer = confirm("yes?");
+console.log(answer);
+let yes = true
+, no = false;
+if(yes || no)
+{
+    console.log('yes or no ' + yes);
+    console.log('yes or no  NO ' + no);
+}
+
